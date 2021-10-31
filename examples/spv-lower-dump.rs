8c45371e2af696a0ffed3c7ce20c65ae8767be68
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-10-31 23:31:20 +0000

    Split out a "dump SPIR-T lowered from SPIR-V" example that doesn't lift back.

---
+++ examples/spv-lower-dump.rs
@@ -0,0 +1,69 @@
+fn main() -> std::io::Result<()> {
+    match &std::env::args().collect::<Vec<_>>()[..] {
+        [_, in_file] => {
+            let module = spirt::Module::lower_from_spv_file(in_file)?;
+
+            let print_operands = |operands: &[_]| {
+                spirt::spv::print::OperandPrinter {
+                    operands: operands.iter(),
+                    out: std::io::stderr().lock(),
+                }
+                .all_operands()
+                .unwrap();
+            };
+
+            let spv_spec = spirt::spv::spec::Spec::get();
+
+            match &module.dialect {
+                spirt::ModuleDialect::Spv(dialect) => {
+                    eprintln!(
+                        "SPIR-V {}.{} module:",
+                        dialect.version_major, dialect.version_minor
+                    );
+
+                    if !dialect.capabilities.is_empty() {
+                        eprintln!("  Capabilities:");
+                        for &cap in &dialect.capabilities {
+                            eprint!("    ");
+                            print_operands(&[spirt::spv::Operand::ShortImm(
+                                spv_spec.well_known.capability,
+                                cap,
+                            )]);
+                            eprintln!();
+                        }
+                    }
+                }
+            }
+
+            eprintln!("  All other instructions:");
+            for top_level in &module.top_level {
+                match top_level {
+                    spirt::TopLevel::SpvInst(inst) => {
+                        eprint!("    ");
+
+                        if let Some(id) = inst.result_id {
+                            eprint!("%{}", id);
+                            if let Some(type_id) = inst.result_type_id {
+                                eprint!(": %{}", type_id);
+                            }
+                            eprint!(" = ");
+                        }
+
+                        eprint!(
+                            "{}",
+                            spv_spec.instructions.get_named(inst.opcode).unwrap().0
+                        );
+                        print_operands(&inst.operands);
+                        eprintln!();
+                    }
+                }
+            }
+
+            Ok(())
+        }
+        args => {
+            eprintln!("Usage: {} IN", args[0]);
+            std::process::exit(1);
+        }
+    }
+}
--- examples/spv-lower-lift-roundtrip.rs
+++ examples/spv-lower-lift-roundtrip.rs
@@ -2,7 +2,13 @@ fn main() -> std::io::Result<()> {
     match &std::env::args().collect::<Vec<_>>()[..] {
         [_, in_file, out_file] => {
             let module = spirt::Module::lower_from_spv_file(in_file)?;
+            module.lift_to_spv_file(out_file)?;
 
+            // FIXME(eddyb) dump the module without reading the just-written file.
+            let parser = spirt::spv::read::ModuleParser::read_from_spv_file(out_file)?;
+
+            // FIXME(eddyb) deduplicate the rest of this function between this
+            // example and `spv-read-write-roundtrip`.
             let print_operands = |operands: &[_]| {
                 spirt::spv::print::OperandPrinter {
                     operands: operands.iter(),
@@ -12,54 +18,35 @@ fn main() -> std::io::Result<()> {
                 .unwrap();
             };
 
+            {
+                // FIXME(eddyb) show more of the header.
+                let [_, v, ..] = parser.header;
+                eprintln!("SPIR-V {}.{} module:", v >> 16, (v >> 8) & 0xff);
+            }
+
             let spv_spec = spirt::spv::spec::Spec::get();
+            for inst in parser {
+                let inst = inst.unwrap();
 
-            match &module.dialect {
-                spirt::ModuleDialect::Spv(dialect) => {
-                    eprintln!(
-                        "SPIR-V {}.{} module:",
-                        dialect.version_major, dialect.version_minor
-                    );
+                eprint!("  ");
 
-                    if !dialect.capabilities.is_empty() {
-                        eprintln!("  Capabilities:");
-                        for &cap in &dialect.capabilities {
-                            eprint!("    ");
-                            print_operands(&[spirt::spv::Operand::ShortImm(
-                                spv_spec.well_known.capability,
-                                cap,
-                            )]);
-                            eprintln!();
-                        }
+                if let Some(id) = inst.result_id {
+                    eprint!("%{}", id);
+                    if let Some(type_id) = inst.result_type_id {
+                        eprint!(": %{}", type_id);
                     }
+                    eprint!(" = ");
                 }
+
+                eprint!(
+                    "{}",
+                    spv_spec.instructions.get_named(inst.opcode).unwrap().0
+                );
+                print_operands(&inst.operands);
+                eprintln!();
             }
 
-            eprintln!("  All other instructions:");
-            for top_level in &module.top_level {
-                match top_level {
-                    spirt::TopLevel::SpvInst(inst) => {
-                        eprint!("    ");
-
-                        if let Some(id) = inst.result_id {
-                            eprint!("%{}", id);
-                            if let Some(type_id) = inst.result_type_id {
-                                eprint!(": %{}", type_id);
-                            }
-                            eprint!(" = ");
-                        }
-
-                        eprint!(
-                            "{}",
-                            spv_spec.instructions.get_named(inst.opcode).unwrap().0
-                        );
-                        print_operands(&inst.operands);
-                        eprintln!();
-                    }
-                }
-            }
-
-            module.lift_to_spv_file(out_file)
+            Ok(())
         }
         args => {
             eprintln!("Usage: {} IN OUT", args[0]);
