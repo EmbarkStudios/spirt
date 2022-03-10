012097842fd6c17d7cd8ec406d145bcfd8e5590d
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2022-03-10 01:04:57 +0000

    print/pretty: replace ad-hoc spaces with a new `Node::BreakingOnlySpace`.

--- src/print/pretty.rs
+++ src/print/pretty.rs
@@ -21,6 +21,16 @@ pub enum Node<'a> {
     PushIndent,
     PopIndent,
 
+    /// Require that nodes before and after this node, are separated by some
+    /// whitespace (either by a single space, or by being on different lines).
+    ///
+    /// This is similar in effect to a `Text(" ")`, except that it doesn't add
+    /// leading/trailing spaces when found at the start/end of a line, as the
+    /// adjacent `\n` is enough of a "breaking space".
+    ///
+    /// Conversely, `Text(" ")` can be considered a "non-breaking space" (NBSP).
+    BreakingOnlySpace,
+
     /// Require that nodes before and after this node, go on different lines.
     ///
     /// This is similar in effect to a `Text("\n")`, except that it doesn't
@@ -52,6 +62,7 @@ impl Node<'_> {
     fn for_single_line(&self) -> &str {
         match self {
             Self::PushIndent | Self::PopIndent => "",
+            Self::BreakingOnlySpace => " ",
             Self::ForceLineSeparation => unreachable!(),
             Self::Text(s) => s,
             Self::Joiner { single_line, .. } => single_line,
@@ -60,7 +71,7 @@ impl Node<'_> {
     fn for_multi_line(&self) -> &str {
         match self {
             Self::PushIndent | Self::PopIndent => "",
-            Self::ForceLineSeparation => "\n",
+            Self::BreakingOnlySpace | Self::ForceLineSeparation => unreachable!(),
             Self::Text(s) => s,
             Self::Joiner { multi_line, .. } => multi_line,
         }
@@ -111,25 +122,33 @@ impl<'a> Fragment<'a> {
             #[derive(Copy, Clone)]
             enum LineOp<'a> {
                 AppendToLine { indent_before: u32, text: &'a str },
+                BreakingOnlySpace,
                 StartNewLine,
                 ForceLineSeparation,
             }
 
             let mut indent = 0;
             let line_ops = self.nodes.iter().flat_map(move |node| {
-                match node {
-                    Node::PushIndent => indent += 1,
+                let (node_text, special_op) = match node {
+                    Node::PushIndent => {
+                        indent += 1;
+                        ("", None)
+                    }
                     Node::PopIndent => {
                         assert!(indent > 0);
                         indent -= 1;
+                        ("", None)
                     }
-                    _ => {}
-                }
-
-                let node_text = if fits_on_single_line {
-                    node.for_single_line()
-                } else {
-                    node.for_multi_line()
+                    Node::BreakingOnlySpace => ("", Some(LineOp::BreakingOnlySpace)),
+                    Node::ForceLineSeparation => ("", Some(LineOp::ForceLineSeparation)),
+                    _ => (
+                        if fits_on_single_line {
+                            node.for_single_line()
+                        } else {
+                            node.for_multi_line()
+                        },
+                        None,
+                    ),
                 };
 
                 node_text
@@ -138,11 +157,8 @@ impl<'a> Fragment<'a> {
                         indent_before: indent,
                         text,
                     })
-                    .intersperse(if let Node::ForceLineSeparation = node {
-                        LineOp::ForceLineSeparation
-                    } else {
-                        LineOp::StartNewLine
-                    })
+                    .intersperse(LineOp::StartNewLine)
+                    .chain(special_op)
             });
 
             // When `on_empty_new_line` is `true`, a new line was started, but
@@ -154,6 +170,11 @@ impl<'a> Fragment<'a> {
             // to avoid trailing whitespace on otherwise-empty lines.
             let mut on_empty_new_line = true;
 
+            // Deferred `LineOp::BreakingOnlySpace`, which should turn into a
+            // single space only between two `LineOp::AppendToLine { text, .. }`
+            // (with non-empty `text`), on the same line.
+            let mut pending_breaking_only_space = false;
+
             line_ops.flat_map(move |op| {
                 let indent_before = match op {
                     LineOp::AppendToLine {
@@ -164,8 +185,20 @@ impl<'a> Fragment<'a> {
                     _ => 0,
                 };
 
+                let space_before = match op {
+                    LineOp::AppendToLine { text, .. }
+                        if pending_breaking_only_space && text != "" =>
+                    {
+                        Some(" ")
+                    }
+
+                    _ => None,
+                };
+
                 let text = match op {
                     LineOp::AppendToLine { text, .. } => text,
+
+                    LineOp::BreakingOnlySpace => "",
 
                     // NOTE(eddyb) reuse the last `\n` if no text came after it,
                     // to avoid creating unnecessary empty lines.
@@ -177,9 +210,16 @@ impl<'a> Fragment<'a> {
                 if (indent_before, text) != (0, "") {
                     on_empty_new_line =
                         matches!(op, LineOp::StartNewLine | LineOp::ForceLineSeparation);
+                    pending_breaking_only_space = false;
+                }
+                if !on_empty_new_line && matches!(op, LineOp::BreakingOnlySpace) {
+                    pending_breaking_only_space = true;
                 }
 
-                (0..indent_before).map(|_| INDENT).chain([text])
+                (0..indent_before)
+                    .map(|_| INDENT)
+                    .chain(space_before)
+                    .chain([text])
             })
         };
         let mut combined = String::with_capacity(mk_reindented_nodes().map(|s| s.len()).sum());
@@ -198,9 +238,10 @@ pub fn join_space(header: &str, contents: impl IntoIterator<Item = String>) -> F
             .chain(contents.into_iter().flat_map(|entry| {
                 [
                     Node::Joiner {
-                        single_line: " ",
+                        single_line: "",
                         multi_line: "\n",
                     },
+                    Node::BreakingOnlySpace,
                     entry.into(),
                 ]
             }))
@@ -216,31 +257,32 @@ pub fn join_comma_sep<'a>(
     contents: impl IntoIterator<Item = impl Into<Fragment<'a>>>,
     suffix: &'a str,
 ) -> Fragment<'a> {
+    let mut contents: SmallVec<[_; 8]> = contents.into_iter().map(Into::into).collect();
+
+    if let Some((last_fragment, non_last_fragments)) = contents.split_last_mut() {
+        for non_last_fragment in non_last_fragments {
+            non_last_fragment
+                .nodes
+                .extend([",".into(), Node::BreakingOnlySpace]);
+        }
+
+        // Trailing comma is only needed after the very last element.
+        last_fragment.nodes.push(Node::Joiner {
+            single_line: "",
+            multi_line: ",",
+        });
+    }
+
     Fragment::new(
         [prefix.into(), Node::PushIndent]
             .into_iter()
-            .chain(
-                contents
-                    .into_iter()
-                    .map(|entry| {
-                        Fragment::new(
-                            iter::once(Node::Joiner {
-                                single_line: "",
-                                multi_line: "\n",
-                            })
-                            .chain(entry.into().nodes)
-                            .chain([Node::Joiner {
-                                single_line: "",
-                                multi_line: ",",
-                            }]),
-                        )
-                    })
-                    .intersperse(Fragment::new([Node::Joiner {
-                        single_line: ", ",
-                        multi_line: "",
-                    }]))
-                    .flat_map(|fragment| fragment.nodes),
-            )
+            .chain(contents.into_iter().flat_map(|fragment| {
+                iter::once(Node::Joiner {
+                    single_line: "",
+                    multi_line: "\n",
+                })
+                .chain(fragment.nodes)
+            }))
             .chain([
                 Node::PopIndent,
                 // FIXME(eddyb) this causes `prefix + "\n" + suffix` output when
