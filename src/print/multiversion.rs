5e8ddd2147243b8e9d1c435259a414e0c056b50d
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2023-06-07 17:06:52 +0300

    print/multiversion: use horizontal scroll if a cell overflows.

--- src/print/multiversion.rs
+++ src/print/multiversion.rs
@@ -123,6 +123,17 @@ impl Versions<pretty::FragmentPostLayout> {
         vertical-align: top;
         max-width: MAX_LINE_WIDTHch;
     }
+    SCOPE>tbody>tr>td>pre {
+        overflow-x: auto;
+
+        /* HACK(eddyb) this shouldn't be needed but `visible` will turn into
+           `auto` because `overflow-x` is `auto` (for cursed browser reasons),
+           and some table cells are always assumed to need vertical scroll,
+           e.g. because they have `<sub>` elements on the last line (which don't
+           contribute to the surrounding bounding box, due to `line-height: 0`)
+         */
+        overflow-y: hidden;
+    }
 </style>
         "
                     .replace("SCOPE", &format!("table.{TABLE_CLASS_NAME}"))
